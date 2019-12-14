f4b8ddd249b1b4600398c27a44039a29a3a333a9
Dan Foley <dan@dan-foley.com>
2019-12-14 20:23:21 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -54,9 +54,10 @@ def total_gross(source)
   d = 0
   total_gross = 0
   while d < list_of_directors.length do
-  
+    total_gross += directors_totals[list_of_directors[d]]
+    d += 1
   end
-  
+  total_gross
 end
 
 
